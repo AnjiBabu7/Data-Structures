fb6b1c7fe5bed5e0451cf3e316f4a42c7112decf
Anjibabu <julurianjibabu7@gmail.com>
2024-07-05 13:12:08 +0530

    binary tree traversals

---
+++ binary_tree.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+struct node{
+int val;
+struct node* right;
+struct node *left;
+node(int x) : val(x), right(NULL),left(NULL) {}
+
+};
+void inorder(node *root){
+    if(root!=NULL){
+        inorder(root->left);
+        cout<<root->val;
+        inorder(root->right);
+    }
+}
+void preorder(node *root){
+    if(root!=NULL){
+        cout<<root->val;
+        preorder(root->left);
+        
+        preorder(root->right);
+    }
+}
+int main(){
+node * root=new node(5);
+node * node1=new node(4);
+node *node2=new node(2);
+node *node3=new node(3);
+node *node4=new node(1);
+node *node5=new node(6);
+root->left=node1;
+node1->left=node2;
+node2->left=node4;
+node1->right=node3;
+root->right=node5;
+inorder(root);
+cout<<endl;
+preorder(root);
+return 0;
+}
